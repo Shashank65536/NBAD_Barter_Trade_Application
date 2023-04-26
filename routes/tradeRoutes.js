29410e9e6a62f67d77f565482808e423c6aa6e60
unknown <shashankbidwai6@gmail.com>
2023-04-25 20:43:38 -0400

    Milestone-4: added db json files

--- routes/tradeRoutes.js
+++ routes/tradeRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const { isLoggedIn } = require('../middleware/auth');
 const router = express.Router();
 
 const controller = require('../controllers/tradeController');
@@ -9,26 +9,26 @@ router.get('/new', (req,res) =>{
     res.send('Send a new form');
 });
 
-router.get('/tradeCategories', controller.tradeCategories);
+router.get('/tradeCategories',isLoggedIn, controller.tradeCategories);
 
-router.get('/allItems', controller.showAllItems);
+router.get('/allItems',isLoggedIn, controller.showAllItems);
 
-router.get('/tradeCategories/:category_id', controller.displayCategoryItems);
+router.get('/tradeCategories/:category_id', isLoggedIn,controller.displayCategoryItems);
 
-router.get('/showTrade/:itemId', controller.getItemDetails);
+router.get('/showTrade/:itemId',isLoggedIn, controller.getItemDetails);
 
 // router.get('/:id',(req,res) =>{
 //     res.send('Send the item with the id');
 // });
 
-router.get('/:id/edit', controller.edit);
+router.get('/:id/edit',isLoggedIn, controller.edit);
 
-router.put('/:id', controller.update);
+router.put('/:id',isLoggedIn, controller.update);
 
-router.delete('/:id', controller.deleteItem);
+router.delete('/:id',isLoggedIn, controller.deleteItem);
 
-router.get('/createForm',controller.create);
+router.get('/createForm',isLoggedIn,controller.create);
 
-router.post('/',controller.save);
+router.post('/',isLoggedIn,controller.save);
 
 module.exports = router;
